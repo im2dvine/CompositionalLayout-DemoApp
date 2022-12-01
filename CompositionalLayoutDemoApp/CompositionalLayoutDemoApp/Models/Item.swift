d821bd721638f181af5477f3db939e79d5d321e3
Daesy <daesy@edfarm.org>
2022-12-01 10:16:33 -0600

    created basic structure for project

---
+++ CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Models/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  CompositionalLayoutDemoApp
+//
+//  Created by Daesy Vences on 11/30/22.
+//
+
+import Foundation
---
+++ CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Models/Section.swift
@@ -0,0 +1,8 @@
+//
+//  Section.swift
+//  CompositionalLayoutDemoApp
+//
+//  Created by Daesy Vences on 12/1/22.
+//
+
+import Foundation
