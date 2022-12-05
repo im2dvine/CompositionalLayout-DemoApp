2e4c4a9eb5251fc6235259fa48caa698744d98cd
Daesy <daesy@edfarm.org>
2022-12-04 23:34:28 -0600

    updated header cell and updated home view controller to display header cell

--- CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Controllers/HomeViewController.swift
+++ CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Controllers/HomeViewController.swift
@@ -2,12 +2,95 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    private var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    lazy var collectionViewLayout: UICollectionViewLayout = {
+        
+        let layout = UICollectionViewCompositionalLayout { [weak self] (sectionIndex, enviroment) ->
+            NSCollectionLayoutSection? in
+            guard let self = self else { return nil }
+            
+            let snapshot = self.dataSource.snapshot()
+            let sectionType = snapshot.sectionIdentifiers[sectionIndex].type
+            
+            switch sectionType {
+            case .header:
+                let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(375), heightDimension: .absolute(125))
+                
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                
+                let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(375), heightDimension: .fractionalHeight(1))
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+                group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .groupPaging
+                section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0)
+                
+                return section
+                
+            default: return nil
+            }
+        }
+        
+        return layout
+        
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initialize()
+    }
+    
+    func initialize() {
+        setUpCollectionView()
+        configureDataSource()
     }
 
+    private func setUpCollectionView() {
+        collectionView.register(UINib(nibName: "HeaderCell", bundle: .main), forCellWithReuseIdentifier: "HeaderCell")
+    
+        
+        collectionView.collectionViewLayout = collectionViewLayout
+    }
+    
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView) { [weak self]
+            (collectionView, indexPath, item) in
+            guard let self = self else { return UICollectionViewCell() }
+            
+            let  snapshot = self.dataSource.snapshot()
+            let sectionType = snapshot.sectionIdentifiers[indexPath.section].type
+            
+            switch sectionType {
+            case .header:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HeaderCell", for: indexPath)
+                return cell
+                
+            default: return nil
+                
+            }
+            
+        }
+        
+        let sections = [
+            Section(type: .header, items: [
+                Item()
+            ]),
+        
+        ]
+        
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections(sections)
+        sections.forEach {snapshot.appendItems($0.items, toSection: $0) }
+        dataSource.apply(snapshot, animatingDifferences: false)
+        
+    }
+    
 }
 
