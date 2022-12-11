fb9dd070c50cae003974db61c5161544baf2d6ac
Daesy <daesy@edfarm.org>
2022-12-11 11:37:00 -0600

    added file for borders for UIViews

---
+++ CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Utilities/UIView+Border.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+@IBDesignable extension UIView {
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let cgColor = layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: cgColor)
+        }
+        set { layer.borderColor = newValue?.cgColor }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+}
