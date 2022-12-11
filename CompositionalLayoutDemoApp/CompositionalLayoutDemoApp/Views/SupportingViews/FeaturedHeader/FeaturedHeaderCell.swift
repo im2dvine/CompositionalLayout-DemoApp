50229336b8e2f45c9689f3601292c240f9661a4e
Daesy <daesy@edfarm.org>
2022-12-11 11:49:38 -0600

    added sliderbars and featuredheadercell nibs

---
+++ CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Views/SupportingViews/FeaturedHeader/FeaturedHeaderCell.swift
@@ -0,0 +1,17 @@
+//
+//  FeaturedHeaderCell.swift
+//  CompositionalLayoutDemoApp
+//
+//  Created by Daesy Vences on 12/11/22.
+//
+
+import UIKit
+
+class FeaturedHeaderCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Views/SupportingViews/SliderBars/SliderBarsCell.swift
@@ -0,0 +1,17 @@
+//
+//  SliderBarsCell.swift
+//  CompositionalLayoutDemoApp
+//
+//  Created by Daesy Vences on 12/11/22.
+//
+
+import UIKit
+
+class SliderBarsCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
