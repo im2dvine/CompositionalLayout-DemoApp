b39b2d24c1db4742bf906f59f095ddcee0aba1e7
Daesy <daesy@edfarm.org>
2023-01-09 20:58:46 -0600

    uodated section and item models in order to be able to add json

--- CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Models/Item.swift
+++ CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Models/Item.swift
@@ -1,14 +1,8 @@
 import Foundation
 
-struct Item: Hashable {
-    let id = UUID()
+struct Item: Decodable, Hashable {
+    let id: Int
     let title: String
     let date: String
     let image: String
-    
-    init(title: String = "", date: String = "", image: String = "") {
-        self.title = title
-        self.date = date
-        self.image = image
-    }
 }
--- CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Models/Section.swift
+++ CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Models/Section.swift
@@ -1,53 +1,9 @@
 import Foundation
 
-struct Section: Hashable {
-    let id = UUID()
-    let type: SectionType
+struct Section: Decodable, Hashable {
+    let id: Int
+    let type: String
     let title: String
     let subtitle: String
     let items: [Item]
-    
-    init(type: SectionType, title: String = "", subtitle: String = "", items: [Item] = []) {
-        self.type = type
-        self.title = title
-        self.subtitle = subtitle
-        self.items = items
-    }
-    
-    enum ItemSectionType: String {
-        case header
-        case searchbar
-        case deals
-        case dotslider
-        case categories
-        case featuredheader
-        case featured
-        case imageheader
-        case details
-        case variant
-        case specifications
-        case cart
-    }
-    
-    struct SectionType: RawRepresentable, Hashable {
-        typealias RawValue = String
-        var rawValue: String
-        
-        init(rawValue: String) {
-            self.rawValue = rawValue
-        }
-        
-        static let header = SectionType(rawValue: Section.ItemSectionType.header.rawValue)
-        static let searchbar = SectionType(rawValue: Section.ItemSectionType.searchbar.rawValue)
-        static let deals = SectionType(rawValue: Section.ItemSectionType.deals.rawValue)
-        static let dotslider = SectionType(rawValue: Section.ItemSectionType.dotslider.rawValue)
-        static let categories = SectionType(rawValue: Section.ItemSectionType.categories.rawValue)
-        static let featuredheader = SectionType(rawValue: Section.ItemSectionType.featuredheader.rawValue)
-        static let featured = SectionType(rawValue: Section.ItemSectionType.featured.rawValue)
-        static let imageheader = SectionType(rawValue: Section.ItemSectionType.imageheader.rawValue)
-        static let details = SectionType(rawValue: Section.ItemSectionType.details.rawValue)
-        static let variant = SectionType(rawValue: Section.ItemSectionType.variant.rawValue)
-        static let specifications = SectionType(rawValue: Section.ItemSectionType.specifications.rawValue)
-        static let cart = SectionType(rawValue: Section.ItemSectionType.cart.rawValue)
-    }
 }
