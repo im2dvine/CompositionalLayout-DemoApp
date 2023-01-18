683c64e7e8a739a5b0a0d04fbcf4ed5d885555b5
Daesy <daesy@edfarm.org>
2023-01-18 11:19:12 -0600

    updated DealsCell and FeaturedCell to display my data

--- CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Views/Home/Deals/DealsCell.swift
+++ CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Views/Home/Deals/DealsCell.swift
@@ -1,8 +1,13 @@
 import UIKit
 
-class DealsCell: UICollectionViewCell {
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+class DealsCell: UICollectionViewCell, SelfConfiguringCell {
+    static let kind = String(describing: DealsCell.self)
+    static let reuseIdentifier = String(describing: DealsCell.self)
+    static let nib = UINib(nibName: String(describing: DealsCell.self), bundle: nil)
+    
+    @IBOutlet weak var dealImage: UIImageView!
+    
+    func configure(with media: Item) {
+        dealImage.image = UIImage(named: media.image)
     }
 }
--- CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Views/Home/Featured/FeaturedCell.swift
+++ CompositionalLayoutDemoApp/CompositionalLayoutDemoApp/Views/Home/Featured/FeaturedCell.swift
@@ -1,8 +1,17 @@
 import UIKit
 
-class FeaturedCell: UICollectionViewCell {
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+class FeaturedCell: UICollectionViewCell, SelfConfiguringCell {
+    static let kind = String(describing: FeaturedCell.self)
+    static let reuseIdentifier = String(describing: FeaturedCell.self)
+    static let nib = UINib(nibName: String(describing: FeaturedCell.self), bundle: nil)
+    
+    @IBOutlet weak var featuredImage: UIImageView!
+    
+    @IBOutlet weak var featuredLabel: UILabel!
+    
+    func configure(with media: Item) {
+        featuredImage.image = UIImage(named: media.image)
+        featuredLabel.text = media.title
     }
+    
 }
